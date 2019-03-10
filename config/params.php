012f3f67794a32a09b2a84358ac0ea6ac36bc357
dhdc1 <dhdctraining@gmail.com>
2019-03-10 12:44:22 +0700

    tehnn

--- config/params.php
+++ config/params.php
@@ -1,11 +1,11 @@
 <?php
-
+$gateway_url = "http://localhost:8001";
 return [
     'adminEmail' => '',
     'hospcode'=>'00000',
-    'hospname'=>'โรงพยาบาลวังทอง',
-    'queue_gateway'=>'http://dc.plkhealth.go.th:8001/queuevisit',
-    'logon_gateway'=>'http://dc.plkhealth.go.th:8001/checkuser',
+    'hospname'=>'โรงพยาบาลแห่งหนึ่ง',
+    'queue_gateway'=>"$gateway_url/queuevisit",
+    'logon_gateway'=>"$gateway_url/checkuser",
     'limit_day'=>15,// จองล่วงหน้าได้กี่วัน
     'tel_cancel'=>'055252052 ต่อ 999'
 ];
